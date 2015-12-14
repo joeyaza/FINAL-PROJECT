58bb5b03f56f88e8abe0dc47eaf00ccf39f803ac
joeyaza <JAzadehdel@gmail.com>
2015-12-14 19:15:39 +0000

    starting the ui.router

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,12 +1,39 @@
 angular
-  .module("ghost-storiesApp", ['satellizer'])
-  .constant('API', 'http://localhost:3000') // your api url here
-  .config(oauthConfig);
+  .module("ghost-storiesApp", ['satellizer', 'ui.router'])
+  .constant('API', 'http://localhost:3000') 
+  .config(oauthConfig)
+  .config(MainRouter);
   
   oauthConfig.$inject = ['API','$authProvider'];
   function oauthConfig(API, $authProvider) {
     $authProvider.facebook({
-      url: API + '/auth/facebook', // the route that will handle the request from facebook
-      clientId: '880329805408488' // your facebook client id
+      url: API + '/auth/facebook', 
+      clientId: '880329805408488' 
     });
+  }
+
+  function MainRouter($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+         url: "/",
+         templateUrl: "./home.html"
+    })
+      .state('newstory', {
+        url: "/newstory",
+        templateUrl: "newstory.html"
+    })
+      .state('story', {
+        url: "/story",
+        templateUrl: "story.html"
+      })
+    //   .state('profile', {
+    //     url: "/",
+    //     templateUrl: "profile.html"
+    // })
+    //   .state('instructions', {
+    //     url: "/instructions",
+    //     templateUrl: "instructions.html"
+
+    $urlRouterProvider.otherwise("/");
   }
--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -47,21 +47,21 @@ function initializeStellar($) {
     });
     //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
     //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
-    function goToByScroll(dataslide) {
-        $htmlbody.animate({
-            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-        }, 2000, 'easeInOutQuint');
-    }
+    // function goToByScroll(dataslide) {
+    //     $htmlbody.animate({
+    //         scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+    //     }, 2000, 'easeInOutQuint');
+    // }
     //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
-    $links.click(function (e) {
-        e.preventDefault();
-        dataslide = $(this).attr('data-slide');
-        goToByScroll(dataslide);
-    });
-    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
-    $buttons.click(function (e) {
-        e.preventDefault();
-        dataslide = $(this).attr('data-slide');
-        goToByScroll(dataslide);
-    });
+    // $links.click(function (e) {
+    //     e.preventDefault();
+    //     dataslide = $(this).attr('data-slide');
+    //     // goToByScroll(dataslide);
+    // });
+    // //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+    // $buttons.click(function (e) {
+    //     e.preventDefault();
+    //     dataslide = $(this).attr('data-slide');
+    //     goToByScroll(dataslide);
+    // });
 };
